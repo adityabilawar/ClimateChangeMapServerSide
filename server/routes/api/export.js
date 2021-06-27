cce1064681565eff26d3f44e4a4372740cb9cb73
Blue Putty <65861156+blue-putty@users.noreply.github.com>
2021-06-27 16:21:34 -0400

    added function call to start csv creation

--- server/routes/api/export.js
+++ server/routes/api/export.js
@@ -4,11 +4,11 @@ const fastcsv = require("fast-csv");
 const fs = require("fs");
 const ws = fs.createWriteStream("bezkoder_mongodb_fastcsv.csv");
 
-const data = ...;
-
-fastcsv
-    .write(data, { headers: true })
-    .on("finish", function () {
-        console.log("Write to bezkoder_mongodb_fastcsv.csv successfully!");
-    })
-    .pipe(ws);
+export default function createCSV(data) {
+    fastcsv
+        .write(data, { headers: true })
+        .on("finish", function () {
+            console.log("Write to bezkoder_mongodb_fastcsv.csv successfully!");
+        })
+        .pipe(ws);
+}
