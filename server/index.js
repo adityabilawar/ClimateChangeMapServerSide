971662b9781c8bda559ee32fe721d4bfc94c9c53
Aditya Bilawar <23ab1107@wwprsd.org>
2021-07-01 12:17:33 -0400

    not using nodemailer for now

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const port = process.env.PORT || 5000;
+
 const app = express();
 
-const nodemailer = require("nodemailer");
-const { getMaxListeners } = require('process');
+//const nodemailer = require("nodemailer");
+
 
 //Middleware
-app.use(express.static('src'));
-app.use(cors());
+//app.use(express.static('src'));
 app.use(express.json());
+app.use(cors());
+
 
 const markers = require('./routes/api/markers');
 const download = require('./routes/api/export');
@@ -26,43 +27,7 @@ if (process.env.NODE_ENV === 'production') {
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/dist/index.html'))
 }
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname + '/dist'));
-app.use(express.json());
-
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'src/index.html'));
-  });
-  
-  app.post('/', (req, res)=>{
-        console.log(req.body)
-  
-        const transporter = nodemailer.createTransport({
-          service: 'gmail',
-          auth: {
-            user: 'climapmessage@gmail.com',
-            pass: 'peppapigpizza123!'
-          }
-        })
-  
-        const mailOptions = {
-          from: req.body.email, //sender address
-          to: 'climap.org@gmail.com', // list of receivers
-          subject: `Message from ${req.body.email}: ${req.body.subject}`, // Subject line
-          text: req.body.message
-      };
-    transporter.sendMail(mailOptions, function (err, info) {
-      if(err)
-      {
-        console.log(err);
-      } else {
-        console.log('Email sent: ' + info.response);
-        res.send('success')
-      }
-  });
-  
-  })
+const port = process.env.PORT || 5000;
   
 
 
